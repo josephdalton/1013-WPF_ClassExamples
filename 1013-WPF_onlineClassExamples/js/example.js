858c57e052e860d64d8f0dc5c49fb1c95a1a14f2
Joseph Dalton <jdalton@fullsail.com>
2013-10-07 19:13:34 -0400

    sampleCode_variableLogsPrompts
    Discussion on variables logs and prompts.

--- 1013-WPF_onlineClassExamples/js/example.js
+++ 1013-WPF_onlineClassExamples/js/example.js
@@ -5,3 +5,40 @@
  * Time: 6:37 PM
  * To change this template use File | Settings | File Templates.
  */
+
+
+//prompt('What color is your cat?');
+//console.log('a');
+
+// comments are
+/* Stuff not to be run*/
+
+// Variables
+//var shoeBoxVariable;      //Declares a variable named shoeBoxVariable
+//shoeBoxVariable='Five';   //sets the variable shoeBoxVariable  equal to 5 (Uses 1 '=')
+//console.log(shoeBoxVariable);
+
+
+
+//combining prompts and console.log
+
+//var userData= prompt('How old are you?');                //return a number value
+//console.log(userData);
+//
+//var userNumberOfSiblings=prompt('Number of siblings?');  //return number value
+//
+//
+//var productUserData = userData*userNumberOfSiblings;
+//console.log(userData*userNumberOfSiblings);
+//console.log(productUserData);
+
+var userFirstName = prompt('What is your first name?');
+var userLastName  =prompt('What is your last name?');
+
+
+console.log(userFirstName+userLastName);
+
+
+
+
+
